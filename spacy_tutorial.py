a7cab3578cb06cc9c61fde43c35dc7a83cf0bb94
Sim Virdi <simeransvirdi@gmail.com>
2016-05-27 17:56:50 +0100

    Feat: Parsed text in spacy, looked at tokens

--- spacy_tutorial.py
+++ spacy_tutorial.py
@@ -8,3 +8,26 @@ multiSentence = (u"There is an art, it says, or rather, a knack to flying." \
                  "In the beginning the Universe was created. This has made a lot of people "\
                  "very angry and been widely regarded as a bad move.")
 
+# all you have to do to parse text is this:
+#note: the first time you run spaCy in a file it takes a little while to load up its modules
+parsedData = parser(multiSentence)
+
+# Let's look at the tokens
+# All you have to do is iterate through the parsedData
+# Each token is an object with lots of different properties
+# A property with an underscore at the end returns the string representation
+# while a property without the underscore returns an index (int) into spaCy's vocabulary
+# The probability estimate is based on counts from a 3 billion word
+# corpus, smoothed using the Simple Good-Turing method.
+for i, token in enumerate(parsedData):
+    print("original:", token.orth, token.orth_)
+    print("lowercased:", token.lower, token.lower_)
+    print("lemma:", token.lemma, token.lemma_)
+    print("shape:", token.shape, token.shape_)
+    print("prefix:", token.prefix, token.prefix_)
+    print("suffix:", token.suffix, token.suffix_)
+    print("log probability:", token.prob)
+    print("Brown cluster id:", token.cluster)
+    print("----------------------------------------")
+    if i > 1:
+        break
